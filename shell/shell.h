6687059de736ba7b25eb7eed678b051f2ab9864a
Agh0stt <zscript.team.zs@gmail.com>
2026-04-18 04:48:18 +0000

    Eclipse32 experimental version v1.1.2.rc1

---
+++ shell/shell.h
@@ -0,0 +1,9 @@
+// =============================================================================
+// Eclipse32 - Eclipse Shell (esh) Header
+// =============================================================================
+#pragma once
+#include "../kernel/kernel.h"
+#include <stdarg.h>
+
+void shell_main(void);
+int  shell_run_script(const char *path);
